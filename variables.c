0666b63b64d1990f4673acf2db8002e6fddf128a
Akhil <96526208+Akhil-Tesla@users.noreply.github.com>
2024-08-09 09:35:24 +0530

    Update variables.c

--- variables.c
+++ variables.c
@@ -2,5 +2,11 @@
 int main(){
 int a=10;
 char string[10]="Code";
+float speed = 369.1037534;
+double quarks = 2804.974648778382947892478924998;
+
 printf("\nvalue of a is %d",a);
-printf("\nvalue of string is %s",string);}
+printf("\nvalue of string is %s",string);
+printf("\nThe speed is %f\n",speed);
+printf("The size of quarks is %lf\n",quarks);
+}
